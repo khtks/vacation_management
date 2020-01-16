1e777252cac2a5d9224599b77ca6cb0d43290d2b
khtks <khtks@naver.com>
2020-01-16 14:12:22 +0900

    각 view 별 template 구분을 prefix 대신 폴더로 구분으로 변경

--- application/views/post.py
+++ application/views/post.py
@@ -7,7 +7,7 @@ post_schema = PostSchema()
 
 @post_bp.route('/main')
 def post_main():
-    return render_template('post_main.html')
+    return render_template('post/main.html')
 
 
 @post_bp.route('/create', methods=['POST'])
@@ -21,13 +21,13 @@ def post_create():
             post = Post(title=title, body=body, category=i)
             db.session.add(post)
             db.session.commit()
-            return render_template('post_create.html', post=post_schema.dumps(post))
+            return render_template('post/create.html', post=post_schema.dumps(post))
 
     category = Category(name=request.form['category'])
     post = Post(title=title, body=body, category=category)
     db.session.add(post)
     db.session.commit()
-    return render_template('post_create.html', post=post_schema.dumps(post))
+    return render_template('post/create.html', post=post_schema.dumps(post))
 
 
 @post_bp.route('<topic>/input')
@@ -37,14 +37,14 @@ def post_create_input(topic):
         result = []
         for i in category_list:
             result.append(i.get_name())
-        return render_template('post_create_input.html', category_list=result)
+        return render_template('post/create_input.html', category_list=result)
 
     if topic == "update":
         post_list = Post.query.all()
         result = []
         for i in post_list:
             result.append(i.get_name())
-        return render_template('post_create_input.html', category_list=result)
+        return render_template('post/create_input.html', category_list=result)
 
 
 @post_bp.route('/delete/select_title')
@@ -54,18 +54,14 @@ def select_title():
     result = []
     for i in post_list:
         result.append(i.get_title())
-    return render_template('post_title.html', title=list(set(result)))
+    return render_template('post/title.html', title=list(set(result)))
 
 
 @post_bp.route('/read_all')
 def post_read_all():
-    count = Post.query.count()
     post_list = Post.query.all()
-    # if count == 1:
-    #     return render_template('post_read_all.html', result=post_schema.dumps(post_list[0]))
-    # else:
-    #     # result = post_schema.dumps(post_list, many=True)
-    return render_template('post_read_all.html', result=post_list, hello="world", post_list_str=post_schema.dumps(post_list, many=True))
+
+    return render_template('post/read_all.html', result=post_list)
 
 
 @post_bp.route('/read', methods=['POST'])
@@ -85,8 +81,6 @@ def post_update():
     pass
 
 
-
-
 @post_bp.route('/delete', methods=['POST'])
 def post_delete():
     post_list = db.session.query(Post).filter(Post.title == request.form['title_name']).all()
@@ -94,7 +88,7 @@ def post_delete():
         db.session.delete(i)
     db.session.commit()
 
-    return render_template('post_delete_result.html', result=post_schema.dumps(Post.query.all(), many=True))
+    return render_template('post/delete_result.html', result=post_schema.dumps(Post.query.all(), many=True))
 
 
 @post_bp.route('/delete_all')
@@ -104,8 +98,4 @@ def post_delete_all():
         db.session.delete(i)
     db.session.commit()
 
-    return render_template('post_delete_result.html', result=post_schema.dumps(Post.query.all(), many=True))
-
-@post_bp.route('<test>/id')
-def test(test):
-    return test
+    return render_template('post/delete_result.html', result=post_schema.dumps(Post.query.all(), many=True))
