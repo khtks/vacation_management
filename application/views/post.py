1a89a25cb4a93c72b7c531bbc2d5958e96302cea
khtks <khtks@naver.com>
2020-01-16 16:00:32 +0900

    flask marshmallow setting & testing

--- application/views/post.py
+++ application/views/post.py
@@ -21,40 +21,38 @@ def post_create():
             post = Post(title=title, body=body, category=i)
             db.session.add(post)
             db.session.commit()
-            return render_template('post/create.html', post=post_schema.dumps(post))
+            return render_template('post/create.html', post=post)
 
     category = Category(name=request.form['category'])
     post = Post(title=title, body=body, category=category)
     db.session.add(post)
     db.session.commit()
-    return render_template('post/create.html', post=post_schema.dumps(post))
+    return render_template('post/create.html', post=post)
 
 
 @post_bp.route('<topic>/input')
-def post_create_input(topic):
+def post_input(topic):
     if topic == "create":
         category_list = Category.query.all()
-        result = []
-        for i in category_list:
-            result.append(i.get_name())
-        return render_template('post/create_input.html', category_list=result)
+        # category_list = list(map(lambda x: x.get_name(), category_list))
+        category_list = [x.get_name() for x in category_list]
+
+        return render_template('post/create_input.html', category_list=category_list)
 
     if topic == "update":
         post_list = Post.query.all()
-        result = []
-        for i in post_list:
-            result.append(i.get_name())
-        return render_template('post/create_input.html', category_list=result)
+        # post_list = list(map(lambda x: x.get_name(), post_list))
+        post_list = [x.get_name() for x in post_list]
+
+        return render_template('post/create_input.html', category_list=post_list)
 
 
 @post_bp.route('/delete/select_title')
 @post_bp.route('/read/select_title')
 def select_title():
     post_list = Post.query.all()
-    result = []
-    for i in post_list:
-        result.append(i.get_title())
-    return render_template('post/title.html', title=list(set(result)))
+    result = [x for x in post_list]
+    return render_template('post/title.html', title=str(set(result)))
 
 
 @post_bp.route('/read_all')
@@ -66,14 +64,12 @@ def post_read_all():
 
 @post_bp.route('/read', methods=['POST'])
 def post_read():
-    title_name = request.form['title_name']
-    post_list = db.session.query(Post).filter(Post.title == title_name).all()
+    post_list = Post.query.filter(Post.title == request.form['title_name']).all()
 
     if len(post_list) == 0:
-        return "ERROR", 400
+        return "Not in the Post", 400
 
-    result = post_schema.dumps(post_list, many=True)
-    return result
+    return post_schema.dumps(post_list, many=True)
 
 
 @post_bp.route('/update')
@@ -83,12 +79,13 @@ def post_update():
 
 @post_bp.route('/delete', methods=['POST'])
 def post_delete():
-    post_list = db.session.query(Post).filter(Post.title == request.form['title_name']).all()
+    post_list = Post.query.filter(Post.title == request.form['title_name']).all()
+
     for i in post_list:
         db.session.delete(i)
     db.session.commit()
 
-    return render_template('post/delete_result.html', result=post_schema.dumps(Post.query.all(), many=True))
+    return render_template('post/delete_result.html', result=Post.query.all())
 
 
 @post_bp.route('/delete_all')
@@ -98,4 +95,4 @@ def post_delete_all():
         db.session.delete(i)
     db.session.commit()
 
-    return render_template('post/delete_result.html', result=post_schema.dumps(Post.query.all(), many=True))
+    return render_template('post/delete_result.html', result=Post.query.all())
