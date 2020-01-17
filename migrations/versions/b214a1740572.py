fddc112011ef28b07f40f7f3d2fad5d0f07141b7
khtks <khtks@naver.com>
2020-01-17 11:12:36 +0900

    flask migrate 적용 테스팅 완료

---
+++ migrations/versions/b214a1740572.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b214a1740572
+Revises: 
+Create Date: 2020-01-17 11:05:49.821885
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b214a1740572'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
