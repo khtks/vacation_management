78aede2ea6084e490d4785463429e60a4e71be44
khtks <khtks@naver.com>
2020-01-21 18:57:05 +0900

    model test 파일 생성

---
+++ application/tests/model/test_post.py
@@ -0,0 +1,22 @@
+import pytest
+from application.models.post_model import *
+from application.tests.model.test_category import *
+
+
+@pytest.fixture
+def post(category):
+    post = Post(title="test title", body="test body", category=category)
+    db.session.add(post)
+    db.session.commit()
+    return post
+
+
+def test_create_post_in_db(post):
+    post_in_db = Post.query.get(post.id)
+
+    assert post_in_db
+    assert post_in_db.title == post.title
+
+
+def test_get_name(post):
+    assert post.title == post.get_title()
