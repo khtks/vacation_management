3e93cc245fea85d722787a35055cde9c9bfd23a5
khtks <khtks@naver.com>
2020-01-21 10:57:37 +0900

    model 및 schema 세분화

---
+++ application/schemata/post_schema.py
@@ -0,0 +1,12 @@
+from flask_marshmallow import Marshmallow
+from application import ma
+from application.models.post import *
+
+
+class PostSchema(ma.ModelSchema):
+    class Meta:
+        model = Post
+
+    # @post_load
+    # def make_post(self, data, **kwargs):
+    #     return Post(**data)
